016ab4d477e4b1747999878ef481fb2ae288234d
Alyssa harper <alyssaharper@Alyssas-Air.home>
2021-10-03 12:22:09 -0400

    deploying

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MongoAtlas', {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
 
 app.use(require('./routes/api'));
